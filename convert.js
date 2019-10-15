d1ca59650716f6aecb4cec7d7627a8e983afebfe
Nik Mirza <nik96mirza@gmail.com>
2019-10-15 12:33:30 +0800

    write to json

--- convert.js
+++ convert.js
@@ -2,6 +2,7 @@
 
 const program = require('commander');
 const fs  = require('fs');
+const app = require('./package.json');
 
 program.version(app.version);
 
@@ -9,5 +10,24 @@ program
     .option('-f, --file <csv>', 'csv file to convert to json', 'locale.csv')
     .parse(process.argv);
 
-let content = fs.readFileSync( program.csv, 'utf8');
-console.log(content);
+let content = fs.readFileSync( program.file, 'utf8');
+let lines  = content.split('\r\n').filter(Boolean).splice(1);
+let headers = content.split('\r\n')[0].split(',').splice(1);
+
+var json = {};
+
+for(line of lines){
+    let arr = line.split(',').splice(1);
+    let key = line.split(',')[0];
+    
+    json[key] = {};
+    
+    for(let i = 0; i < headers.length; i++){        
+        json[key][headers[i]] = arr[i];
+    }
+}
+
+fs.writeFile("locale.json", JSON.stringify(json), 'utf8', (err)=>{
+    if(err)
+        console.error(err);
+});
